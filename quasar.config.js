3e7752a1f96311c44b3e859a948e1fb675cb43cb
Sang Huen <sanghuen15@gmail.com>
2025-10-08 10:38:23 +0700

    Edit Path

--- quasar.config.js
+++ quasar.config.js
@@ -35,7 +35,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-       publicPath: '/6740101381-sanghuen-/',
+       publicPath: '/6704101381-Sanghuen-/',
       //vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
